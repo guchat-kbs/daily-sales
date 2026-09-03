1dc5d5dc84f1530c950df849bcec903e4258eb06
SAKAL <karlexblackstudios@gmail.com>
2026-09-03 11:49:00 +0530

    DONE

--- build.sh
+++ build.sh
@@ -7,17 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# --- AUTO CREATE SUPERUSER ---
-python manage.py shell << END
-from django.contrib.auth import get_user_model
-User = get_user_model()
-username = "sakal"
-email = "sakalytshit@gmail.com"
-password = "Salibill1"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print(f"Superuser {username} created!")
-else:
-    print(f"Superuser {username} already exists")
-END
