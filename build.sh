be8024abecf10b8b4cc70330c772708a6f1ea21d
SAKAL <karlexblackstudios@gmail.com>
2026-09-03 14:35:20 +0530

    lets see

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # exit on error
-#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py shell
+python manage.py shell -c "
 from entries.models import Entry
-Entry.objects.filter(owner__isnull=True).count()   # see how many will be deleted
-Entry.objects.filter(owner__isnull=True).delete()   # delete them
-exit()
+deleted_count = Entry.objects.filter(owner__isnull=True).count()
+print(f'Deleting {deleted_count} orphaned entries...')
+Entry.objects.filter(owner__isnull=True).delete()
+"
