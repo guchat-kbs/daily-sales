c6949d3f76677a5fe23d5db26f1e33ff534b857a
SAKAL <karlexblackstudios@gmail.com>
2026-09-03 14:33:27 +0530

    M

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+python manage.py shell
+from entries.models import Entry
+Entry.objects.filter(owner__isnull=True).count()   # see how many will be deleted
+Entry.objects.filter(owner__isnull=True).delete()   # delete them
+exit()
