5cf4f2399162744b90cbc4ff81833cd436bc61ec
SAKAL <karlexblackstudios@gmail.com>
2026-09-04 21:06:07 +0530

    Assam

--- build.sh
+++ build.sh
@@ -10,22 +10,7 @@ python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
 
-username = '${DJANGO_SUPERUSER_USERNAME}'
-email = '${DJANGO_SUPERUSER_EMAIL}'
-password = '${DJANGO_SUPERUSER_PASSWORD}'
-
-if not User.objects.filter(username=username).exists():
-    user = User.objects.create_superuser(
-        username=username,
-        email=email,
-        password=password,
-    )
-    print(f'Superuser {username} created successfully.')
-else:
-    print(f'Superuser {username} already exists.')
-"
 
 python manage.py shell -c "
 from entries.models import Entry
