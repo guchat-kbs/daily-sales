00c3edd2a0b2025506324f7aa27e45da61169e55
SAKAL <karlexblackstudios@gmail.com>
2026-09-04 21:07:51 +0530

    DOne

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+```bash
 #!/usr/bin/env bash
 set -o errexit
 
@@ -8,13 +9,9 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py shell -c "
-from django.contrib.auth import get_user_model
-
-
-
-python manage.py shell -c "
 from entries.models import Entry
 deleted_count = Entry.objects.filter(owner__isnull=True).count()
 print(f'Deleting {deleted_count} orphaned entries...')
 Entry.objects.filter(owner__isnull=True).delete()
 "
+```
