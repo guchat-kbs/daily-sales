672c7545d371e623497c861743198c622760d81d
SAKAL <karlexblackstudios@gmail.com>
2026-09-13 15:54:23 +0530

    Naddi

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,8 @@ DEBUG = os.environ.get(
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "shillongdart.online",
-    "www.shillongdart.online",
+    "assamdart.online",
+    "www.assamdart.online",
     ".onrender.com",
 ]
 
@@ -49,8 +49,8 @@ USE_X_FORWARDED_HOST = True
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://shillongdart.online",
-    "https://www.shillongdart.online",
+    "https://assamdart.online",
+    "https://www.assamdart.online",
 ]
 
 
